387879e0640a29b86b1b5f3d1b63acf35fbc1812
ARTUR0jr <saavedra.arturo.jr@gmail.com>
2022-08-12 14:54:43 +0800

    3 files modified

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 // Import stylesheets
 import './style.css';
 
+import { initializeApp } from 'firebase/app';
+
+// Add the Firebase products and methods that you want to use
+import { getAuth, EmailAuthProvider, signOut,
+  onAuthStateChanged } from 'firebase/auth';
+
+import {getFirestore, addDoc, collection, query, orderBy, onSnapshot, doc, setDoc, where, limit} from 'firebase/firestore';
 import firebase from "firebase"
+import * as firebaseui from 'firebaseui';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
+
+let rsvpListener = null;
+let guestbookListener = null;
+
+let db, auth;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
